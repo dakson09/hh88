14230892811daa26d3cd6d250b92de85e2de5d65
dakson09 <122751385+dakson09@users.noreply.github.com>
2023-01-16 12:50:27 +0200

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-var username = document.getElementById("username").value;
-var password = document.getElementById("password").value;
-
-if (username == "admin" && password == "daddy123") {
-  alert("Login successful!");
-  window.location.href = "image.html"
-} else {
-  alert("Incorrect username or password. Please try again.");
-}
